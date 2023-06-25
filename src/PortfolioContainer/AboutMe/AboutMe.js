0015094740ff8cd211a32bfa1f27b251c401bec3
chandrimak <chandrima.kabiraj@ellucian.com>
2023-06-25 18:15:51 +0530

    Added CV

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -65,7 +65,7 @@ export default function AboutMe(props) {
                 {" "}
                 Hire Me{" "}
               </button>
-              <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+              <a href="Resume.pdf" download="Chandrima_CV.pdf">
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
